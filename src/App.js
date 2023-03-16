bcb43df4f619d2b9c6a58bd48f43efb8c4cf949b
rodrigoemarquez <rodrigoemarquez@hotmail.com>
2023-03-16 14:21:09 +1100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo mb-4' alt='logo'
+ />
+				<p className='mt-4'>Bootstrap has been included and I am ready to go.</p>
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+        <div className='d-flex gap-2'>
+				<Button
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Learn React
+				</Button>
+				<Button
+					className='App-link'
+					href='https://react-bootstrap.github.io'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Learn Bootstrap
+				</Button>
+        </div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
